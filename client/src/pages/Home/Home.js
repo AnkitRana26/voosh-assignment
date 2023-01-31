f71e176f514ed4a9c7c9dc29008ecefbe6250ad8
Ankit Rana <109352490+AnkitRana26@users.noreply.github.com>
2023-01-31 18:25:24 +0530

    UI Done

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Home.css'
+const data = [
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    },
+    {
+        id: 1,
+        sub_total: 500,
+        phone_number: 999999999
+    }
+]
+
+const initialForm={
+    sub_total:''
+}
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [form,setForm]= useState(initialForm);
+    const changeHandler =(e)=>{
+        const {name,value} = e.target;
+        setForm({...form,[name]:value});
+    }
+    return (
+        <div id='homeContainer'>
+            <h1>Orders DashBoard</h1>
+            <div id='mainConatiner'>
+                <div id='orderContainer'>
+                    {
+                        data.map((ele) => {
+                            return <div className='order'>
+                                <h2>₹{ele.sub_total}</h2>
+                                <h3>Mobile:- {ele.phone_number}</h3>
+                            </div>
+                        })
+                    }
+                </div>
+                <div id='addOrderContainer'>
+                    <h2>Add Order</h2>
+                    <form id="addOrderForm">
+                        <input class="inputField" type="number" name="sub_total" placeholder="Enter Sub Total" onChange={changeHandler} value={form.sub_total} />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
